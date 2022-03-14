dfa07522cf23baf63512a66cd367eb874293d577
xersky <ayoub.elaoufi@gmail.com>
2022-03-14 14:39:54 +0100

    Finalizing and optimizing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ReadComponent } from './read/read.component';
 
 const routes: Routes = [
-  { path: 'items', component: ReadComponent}
+  { path: 'items', component: ReadComponent},
 ];
 
 @NgModule({
--- src/app/read/read-items/read-items.component.ts
+++ src/app/read/read-items/read-items.component.ts
@@ -2,7 +2,6 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Item} from '../../models/item.model';
 import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs';
-import {EventService} from '../../services/event.service';
 import * as fromItemActions from '../../state/actions/item.actions';
 
 @Component({
@@ -14,7 +13,7 @@ export class ReadItemsComponent implements OnInit {
 
   @Input() items$: Observable<Item[]> | null = null;
 
-  constructor(private eventService: EventService, private store: Store) {
+  constructor(private store: Store) {
   }
 
   ngOnInit(): void {
